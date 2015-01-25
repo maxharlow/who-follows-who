051b9b406b75091b3a0679af8fe241e19cf80742
Max Harlow <maxharlow@gmail.com>
2015-01-25 14:06:08 +0000

    More data...

--- who-follows-who.py
+++ who-follows-who.py
@@ -15,12 +15,15 @@ with open('accounts.csv', 'rU') as input:
 with open('accounts.csv', 'rU') as input:
     rows = csv.DictReader(input)
     with open('results.csv', 'w') as output:
-        writer = csv.DictWriter(output, ['Account', 'Tweets', 'Followers', 'AccountsFollowingNumber', 'AccountsFollowing'])
+        writer = csv.DictWriter(output, ['Account', 'Tweets', 'Following', 'Followers', 'JoinDate', 'AccountsFollowingNumber', 'AccountsFollowing'])
+        writer.writeheader()
         for row in rows:
             if row['Account'] == '': continue
             detail_response = twitter.api.users.show.get(screen_name=row['Account'])
             tweets = detail_response.data['statuses_count']
-            followers = detail_response.data['friends_count']
+            following = detail_response.data['friends_count']
+            followers = detail_response.data['followers_count']
+            join_date = detail_response.data['created_at']
             following_response = twitter.api.friends.ids.get(screen_name=row['Account'])
             following_ids = list(map(str, following_response.data['ids'])) # extract the id field
             following_ids_grouped = [following_ids[i:i + 100] for i in range(0, len(following_ids), 100)] # grouped by 100
@@ -33,8 +36,10 @@ with open('accounts.csv', 'rU') as input:
             data = {
                 'Account': row['Account'],
                 'Tweets': tweets,
+                'Following': following,
                 'Followers': followers,
-                'AccountsFollowingNumber': len(following_accounts)
+                'JoinDate': join_date,
+                'AccountsFollowingNumber': len(following_accounts),
                 'AccountsFollowing': ', '.join(following_accounts)
             }
             writer.writerow(data)
